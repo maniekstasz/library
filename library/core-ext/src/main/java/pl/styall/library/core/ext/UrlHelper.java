4b31af6936cf0b88321b0e8eadc5667466c6a415
szymon <konik32@gmail.com>
2014-03-11 14:49:28 +0100

    Dodałem UrlHelper.java

---
+++ library/core-ext/src/main/java/pl/styall/library/core/ext/UrlHelper.java
@@ -0,0 +1,16 @@
+package pl.styall.library.core.ext;
+
+import org.apache.commons.lang3.StringUtils;
+
+
+
+public class UrlHelper {
+
+	
+	public static String getUrlSafeString(String str){
+		String abbr = StringUtils.abbreviate(str, 70);
+		String withoutAccent = StringUtils.stripAccents(abbr);
+		String replacedOhter = withoutAccent.replaceAll("[\\W]","_");
+		return replacedOhter;
+	}
+}
